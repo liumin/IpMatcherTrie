0157fdd72d8289cb704fc07a22d5cc56940ebda4
unknown <limimi.33@163.com>
2011-08-10 10:02:18 +0800

    IpMaterTree

---
+++ src/cn/edu/sjtu/front/iputils/IpMatchConf.java
@@ -0,0 +1,15 @@
+package cn.edu.sjtu.front.iputils;
+/**
+ * 
+ */
+
+
+/**
+ * @author jianwen
+ *
+ */
+public abstract class IpMatchConf {
+	
+	public static final String NETFILE = "./net.txt";
+	
+}
